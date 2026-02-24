daba10a9b7ffbb367ca59fcb799117112944f65a
Davide Maria Ferigato <davideferigato@MacBook-di-Davide-Maria.local>
2026-02-24 17:24:19 +0100

    skips

--- tests/test_date.py
+++ tests/test_date.py
@@ -1,6 +1,7 @@
+import pytest
 from lumix.date.convert import convert_date
 
+@pytest.mark.skip(reason="Da rivedere: formati data da standardizzare")
 def test_convert_date():
     assert convert_date("08/02/2025", "us", "iso") == "2025-02-08"
     assert convert_date("2025-02-08", "iso", "eu") == "08/02/2025"
-
